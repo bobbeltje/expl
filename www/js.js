8c3513823ecf74c7c749c544c32a02713bc3b6b7
bobbel <matthiaswubs@gmail.com>
2021-07-17 18:50:04 +0200

    close button

--- www/js.js
+++ www/js.js
@@ -4,9 +4,42 @@ var data = {
     x: [...Array(n)].map(_ => Math.ceil(Math.random() * n)),
     z: [...Array(n)].map(_ => Math.ceil(Math.random() * n))
 }
+var counter = 0;
 
-function plot(id){
-    let gd = document.getElementById(id);
+function explore(){
+    new_data();
+    let gd = document.getElementById('plt');
+
+    // plot div
+    let pd = document.createElement('div');
+    pd.id = 'tmp';
+    pd.className = 'plot-container';
+
+    // close button
+    let bd = document.createElement('button');
+    bd.className = 'test-button';
+    bd.innerText = 'x';
+    bd.addEventListener(
+        'click',
+        () => {
+            console.log(counter += 1);
+            document.getElementById('tmp').remove();
+            document.querySelector('.test-button').remove();
+        }
+    );
+    
+    gd.replaceChildren(pd, bd);
+    plot();
+}
+
+function new_data(){
+    data.x = [...Array(n)].map(_ => Math.ceil(Math.random() * n));
+    data.y = [...Array(n)].map(_ => Math.ceil(Math.random() * n));
+    data.z = [...Array(n)].map(_ => Math.ceil(Math.random() * n));
+}
+
+function plot(){
+    let gd = document.getElementById('tmp');
 
     let d = [{
         type: 'scatter',
